2de53de8919a245d28a58c2cd9a577490c820173
krunal <krunal.maru1106@gmail.com>
2023-01-16 16:39:44 +0530

    register and login user

--- eshop/app/urls.py
+++ eshop/app/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from app import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
-    path('', views.home, name="home")
-]
+    path('', views.home, name="home"),
+    path('accounts/myaccount/', views.myaccount, name="myaccount"),
+    path('accounts/registration/', views.register, name='register'),
+    path('accounts/login/', views.userlogin, name='userlogin'),
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- eshop/app/views.py
+++ eshop/app/views.py
@@ -1,7 +1,46 @@
-from django.shortcuts import render
-from .models import Category,Subcategory
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from .models import Category,Subcategory, Product
 # Create your views here.
 def home(request):
     category = Category.objects.all()
-    context = {'category':category}
+    categoryid = request.GET.get('category')
+    if categoryid:
+        product = Product.objects.filter(subcategory=categoryid).order_by('-id')
+    else:
+        product = Product.objects.all()
+        
+    context = {'category':category, 'product':product}
     return  render(request,'app/home.html',context)
+
+def myaccount(request):
+    return render(request, 'accounts/registration.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'Email Already Exist')
+            return redirect('register')
+        user = User(username=username,email=email)
+        user.set_password(password)
+        user.save()
+        return redirect('home')
+
+def userlogin(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request,username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid Username and Password')
+            return redirect('userlogin')
