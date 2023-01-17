5e352bf91b0c89f0febf69d11dc78743279f37a0
krunal <krunal.maru1106@gmail.com>
2023-01-17 18:15:15 +0530

    create contact page

--- eshop/app/models.py
+++ eshop/app/models.py
@@ -26,3 +26,13 @@ class Product(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+class Contactus(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    subject = models.CharField(max_length=100)
+    message = models.TextField()
+
+    def __str__(self) -> str:
+        return self.name
+
